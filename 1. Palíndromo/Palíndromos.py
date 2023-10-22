288e5ac344409e596eb70f8748be5050c0c88c13
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-10-21 19:22:53 -0600

    Primer código de ejercicio

---
+++ 1. Palíndromo/Palíndromos.py
@@ -0,0 +1,16 @@
+# Declaracion de variable Palabra
+Palabra = str(input("Dame una palabra para saber si es un palíndromo: "))
+
+# Función para comparar la palabra
+def Palindromo(cadena):
+    # Convierte la cadena a minúsculas y quita los espacios en blanco
+    cadena = cadena.lower().replace(" ", "")
+    # Compara la cadena original con su inversa y regresa la funcion
+    return cadena == cadena[::-1]
+
+# Codigo principal
+# Resultado de comparacion usando un If
+if Palindromo(Palabra):
+    print(f"{Palabra} es un palíndromo")
+else:
+    print(f"{Palabra} no es un palíndromo")
